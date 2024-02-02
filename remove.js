1ffdd216db1627eb3300c60e0d6dfc33273eb1f8
xnanyt <142925435+Eurie92@users.noreply.github.com>
2024-02-02 23:15:34 +0800

    Add files via upload

---
+++ remove.js
@@ -0,0 +1,12 @@
+function reMove() {
+    const hearts = document.getElementById('heart');
+    const shows = document.getElementById('show');
+    const gone = document.getElementById('see');
+  
+
+
+    gone.style.display = 'none';
+    hearts.style.display = 'none';
+    shows.style.display = 'none';
+
+}
